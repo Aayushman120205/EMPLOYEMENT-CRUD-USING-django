8489db69e6e1772b691b060e5c1efe9efa8576b3
Aayushman <143834698+Aayushman120205@users.noreply.github.com>
2025-08-11 22:33:20 +0530

    Delete Login directory

--- Login/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import  path
-from .views import EmployeeCreateView,EmployeeListView,EmployeeDeleteByIdView,EmployeeGetByIdView
-
-urlpatterns = [
-    path('getall-employees/', EmployeeListView.as_view(), name='get-all-employees'),
-    path('create-employees/', EmployeeCreateView.as_view(), name='create-employee'),
-    path('get-employees/<int:pk>/', EmployeeGetByIdView.as_view(), name='get-employee-by-id'),
-    path('delete-employees/<int:pk>/', EmployeeDeleteByIdView.as_view(), name='delete-employee-by-id'),
-]
